19251fb195d9834fa27d86dc8f9e46365864ad14
dchudik <d.chudinov.spb@yandex.ru>
2018-03-08 23:37:21 +0300

    This main script

---
+++ start.py
@@ -0,0 +1,12 @@
+#This is script for generate code for xss attack
+
+print("""
+*   * ****       *     *  *****  **** ***** **** *****
+ * *  *          * * * * *     * *      *   *    *   *
+  *   ****  ***  *  *  * ******* ****   *   **** *****
+ * *     *       *     * *     *    *   *   *    * *
+*   * ****       *     * *     * ****   *   **** *   *
+
+Author: Dima Chudinov
+GitHUB: dchudik
+""")
